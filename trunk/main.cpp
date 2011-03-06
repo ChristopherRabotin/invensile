ae27edb92265c03b7a61a6bd62e546d2e38c9806
christopher.rabotin@gmail.com <christopher.rabotin@gmail.com@9a7a25ce-5462-25d4-2179-68bcd4c7d05c>
2011-03-06 12:40:17 +0000

    Partially correct previsouly mentioned bug. Committing before major GUI change.

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -1,4 +1,5 @@
 #include <QtGui/QApplication>
+#include <QDesktopWidget>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
@@ -6,5 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    /* Moves the window to the center of the screen. */
+    w.move(qApp->desktop()->availableGeometry(qApp->desktop()).center()-w.rect().center());
     return a.exec();
 }
